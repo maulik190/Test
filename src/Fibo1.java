71699582f05d96e0dd322679e413cadef1f9ae7b
Maulik <mauliks99@gmail.com>
2018-01-23 07:08:24 -0500

    test one

---
+++ src/Fibo1.java
@@ -0,0 +1,33 @@
+
+public class Fibo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int fiboLimit = 5;
+		int fibo;
+
+		for (int i = 0; i < fiboLimit; i++) {
+			int first = 0;
+			int second = 1;
+			int next;
+
+			if (i == 0) {
+				next = 0;
+			}
+
+			if (i == 1) {
+				next = 1;
+			} else {
+
+				next = first + second;
+				first = second;
+				second = next;
+			}
+
+			System.out.println("Series Is: " + next);
+
+		}
+	}
+
+}
